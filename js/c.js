4e20349a14525df6bd66b5ad268054dfcb445521
Philip Kahn <tigerhawkvok@gmail.com>
2017-01-18 13:22:35 -0800

    Change breakpoint on resizer

--- js/c.js
+++ js/c.js
@@ -788,7 +788,7 @@
       $("nav#toc").css("top", topOffset);
       return topOffset;
     };
-    if ($(window).width() <= 1280) {
+    if ($(window).width() <= 1367) {
       minHeight = $("nav#toc").outerHeight(true);
       topOffset = scrollOffsetter();
       $(window).scroll(scrollOffsetter);
